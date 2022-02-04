ae3efbc433edc58c293224dadb7332c696762aa2
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2022-02-04 14:48:06 +0200

    шаблоны конструкций

--- crm/views.py
+++ crm/views.py
@@ -1,4 +1,8 @@
 # контроллер приложения
 from django.shortcuts import render
+from .models import Order
 
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', { 'object_list': object_list })
