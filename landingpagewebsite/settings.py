27c0a31078230d390bbb2592aeb3adeeac170e12
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2022-03-04 19:31:56 +0200

    статические файлы и медия директории

--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'. (настройки директории)
@@ -116,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images) (статические файлы)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "landingpagewebsite/static/",
+]
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
